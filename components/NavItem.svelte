cfb6699edd842d78220e5fe819776cf18631ffe2
Richard Harris <richard.a.harris@gmail.com>
2019-04-28 14:33:49 -0400

    add some more components etc

---
+++ components/NavItem.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { getContext } from 'svelte';
+
+	export let segment = null;
+	export let external = null;
+
+	const current = getContext('nav');
+</script>
+
+{#if external}
+	<li><a href={external}><slot></slot></a></li>
+{:else}
+	<li class:active="{$current === segment}"><a rel="prefetch" href={segment}><slot></slot></a></li>
+{/if}
