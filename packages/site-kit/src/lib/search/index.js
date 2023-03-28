bf4815293eb9ba690cddc95a4983db3eea582d45
Simon Holthausen <simon.holthausen@vercel.com>
2023-03-28 12:40:29 +0200

    fix: import name

--- packages/site-kit/src/lib/search/index.js
+++ packages/site-kit/src/lib/search/index.js
@@ -1,5 +1,5 @@
 export { default as Search } from './Search.svelte';
-export { default as Searchbox } from './Searchbox.svelte';
+export { default as SearchBox } from './SearchBox.svelte';
 export { default as SearchResults } from './SearchResults.svelte';
 export { init, inited, lookup, search } from './search.js';
 
