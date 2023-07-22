bec30fc646a141de69d03d40372566e9bc7826f6
Brent Morton <55322556+b1mind@users.noreply.github.com>
2023-07-22 03:12:45 -0600

    fix: home link space (#163)
    * Update Nav.svelte
    
    Change to make home link "dense" and not fill blank space.
    
    * change: justify to max-width
    
    better support for home-line on both desktop and mobile.
    
    ---------
    
    Co-authored-by: Puru Vijay <47742487+PuruVJ@users.noreply.github.com>

--- packages/site-kit/src/lib/nav/Nav.svelte
+++ packages/site-kit/src/lib/nav/Nav.svelte
@@ -197,6 +197,7 @@ Top navigation bar for the application. It provides a slot for the left side, th
 	}
 
 	.home-link {
+		max-width: max-content;
 		height: 100%;
 		display: flex;
 		background-image: url(../branding/svelte-logo.svg);
