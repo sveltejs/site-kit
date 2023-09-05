3272589516252abc57f7f64d94ab3feec13c9d4d
Puru <devpuruvj@gmail.com>
2023-09-05 15:10:20 +0530

    Push

--- packages/site-kit/src/lib/nav/index.js
+++ packages/site-kit/src/lib/nav/index.js
@@ -2,3 +2,4 @@ export { default as Nav } from './Nav.svelte';
 export { default as PreloadingIndicator } from './PreloadingIndicator.svelte';
 export { default as Separator } from './Separator.svelte';
 export { default as SkipLink } from './SkipLink.svelte';
+export { open_nav } from './Menu.svelte';
