b60e2fca9221feaa202b7177c7a323621f4f6a77
Puru <devpuruvj@gmail.com>
2023-09-05 15:17:23 +0530

    Sort imports

--- packages/site-kit/src/lib/nav/index.js
+++ packages/site-kit/src/lib/nav/index.js
@@ -1,5 +1,5 @@
+export { open_nav } from './Menu.svelte';
 export { default as Nav } from './Nav.svelte';
 export { default as PreloadingIndicator } from './PreloadingIndicator.svelte';
 export { default as Separator } from './Separator.svelte';
 export { default as SkipLink } from './SkipLink.svelte';
-export { open_nav } from './Menu.svelte';
