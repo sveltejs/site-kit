6b5f89f03211ae11bb50ee445f626487c4735bb2
Puru <devpuruvj@gmail.com>
2023-09-22 15:59:03 +0530

    Remove export

--- packages/site-kit/src/lib/stores/index.js
+++ packages/site-kit/src/lib/stores/index.js
@@ -2,5 +2,4 @@ export { mql } from './mql.js';
 export { nav_open, on_this_page_open, overlay_open, should_nav_autohide } from './nav.js';
 export { reduced_motion } from './reduced-motion.js';
 export { search_query, search_recent, searching } from './search.js';
-export { banner_count } from './shell.js';
 export { theme } from './theme.js';
