8db95a1135d3b622cb42fee40cd881f778066c7a
Puru Vijay <47742487+PuruVJ@users.noreply.github.com>
2023-12-06 22:48:10 +0530

    chore: Refactor banner, stricter (#259)
    * Refactor banner
    
    * More refactoring, banner is now a pure component
    
    * defineBanner
    
    * Push changeset

--- packages/site-kit/src/lib/components/Banners.svelte
+++ packages/site-kit/src/lib/components/Banners.svelte
@@ -1,12 +1,44 @@
 <script context="module">
 	/**
+	 * @typedef {'svelte.dev' | 'kit.svelte.dev' | 'learn.svelte.dev'} BannerScope
+	 *
+	 * @typedef {{
+	 * id: string;
+	 * start: number;
+	 * end: number;
+	 * arrow: boolean;
+	 * href: string;
+	 * content: string;
+	 * scope?: BannerScope[];
+	 * }[]} BannerData
+	 *
+	 * @typedef {(Omit<BannerData[0], 'start' | 'end'> & { start: Date, end?: Date })[]} BannerDataInput
+	 */
+
+	/**
 	 * Only to be used on non svelte.dev sites. Shouldn't be used inside svelte.dev codebase itself
-	 * @param {import('./Banner.svelte').BannerScope} scope
+	 * @param {BannerScope} scope
 	 * @param {import('@sveltejs/kit').RequestEvent['fetch']} fetch
-	 * @returns {Promise<import('./Banner.svelte').BannerData>}
+	 * @returns {Promise<BannerData>}
 	 */
 	export async function fetchBanner(scope = 'svelte.dev', fetch) {
-		if (scope === 'svelte.dev') return fetch('/banner.json').then((r) => r.json());
+		if (scope === 'svelte.dev') {
+			const data = /** @type {BannerData} */ (await fetch('/banner.json').then((r) => r.json()));
+
+			// Find out if any time overlap will happen in any banner.
+			// If so, throw an error.
+			// This is to prevent showing 2 banners at once at any point of time.
+			for (const banner of data) {
+				for (const other of data) {
+					if (banner.id === other.id) continue;
+					if (banner.start < other.end && banner.end > other.start) {
+						throw new Error(
+							`svelte.dev/banner.json: Banner with ID ${banner.id} and ${other.id} are overlapping.`
+						);
+					}
+				}
+			}
+		}
 
 		const req = await fetch('https://svelte.dev/banner.json');
 		if (!req.ok) {
@@ -14,21 +46,58 @@
 			return [];
 		}
 
-		return /** @type {import('./Banner.svelte').BannerData} */ (await req.json()).filter(
+		return /** @type {BannerData} */ (await req.json()).filter(
 			(banner) => !banner.scope || banner.scope?.includes(scope)
 		);
+	}
+
+	/**
+	 * @param {BannerDataInput} data
+	 * @returns {BannerData}
+	 */
+	export function defineBanner(data) {
+		return data.map((v) => ({
+			...v,
+			start: +v.start,
+			end: v.end ? +v.end : +new Date('2023-12-01')
+		}));
 	}
 </script>
 
 <script>
+	import { browser } from '$app/environment';
+	import { onMount } from 'svelte';
+	import { persisted } from 'svelte-local-storage-store';
 	import Banner from './Banner.svelte';
 
-	/** @type {import('./Banner.svelte').BannerData} */
+	/** @type {BannerData} */
 	export let data;
+
+	const preferences = persisted(
+		'svelte:banner-preferences',
+		/** @type {Record<string, boolean>} */ ({})
+	);
+
+	const time = +new Date();
+
+	$: showing = data.filter(({ id, start, end }) => $preferences[id] && time > start && time < end);
+
+	$: if (browser) {
+		document.documentElement.style.setProperty(
+			'--sk-banner-bottom-height',
+			showing.length ? '43px' : '0px'
+		);
+	}
+
+	onMount(() => {
+		for (const { id } of data) {
+			$preferences[id] ??= true;
+		}
+	});
 </script>
 
-{#each data as { content, href, id, start, arrow, end }}
-	<Banner {id} start={new Date(start)} end={end ? new Date(end) : undefined} {arrow} {href}>
+{#each showing as { content, href, id, arrow }}
+	<Banner {arrow} {href} on:close={() => ($preferences[id] = false)}>
 		{@html content}
 	</Banner>
 {/each}
